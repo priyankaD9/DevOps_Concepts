bca1a9d3e817ae51a92dd498ed85ec10fa59bf43
Priyanka Dhage <dhagepriyanka938@gmail.com>
2026-03-09 14:13:32 +0000

    Add Introduction folder with hello_world.py

---
+++ Introduction/hello_world.py
@@ -0,0 +1,17 @@
+# -----------------------------------------------------------
+# Simple Hello World Program in Python
+# -----------------------------------------------------------
+# Python is an interpreted programming language widely used
+# for automation, scripting, DevOps tools, and backend development.
+#
+# print() is a built-in Python function used to display output
+# on the terminal/console.
+#
+# Unlike some languages (like Go or Java), Python does not
+# require a main function to start execution. The program runs
+# from top to bottom.
+# -----------------------------------------------------------
+
+# Display a message on the console
+
+print("Hello World")
