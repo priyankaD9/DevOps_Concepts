8418ea1ed103abe6107f326bd01390871e4e3373
Priyanka Dhage <dhagepriyanka938@gmail.com>
2026-03-11 13:31:24 +0000

    Add python datatypes and variables

---
+++ Python/Python_Tasks/DataTypes & Variables/var.py
@@ -0,0 +1,26 @@
+# datatypes.py
+# This file explains different data types in Python in a simple way
+
+# String
+name = "Priyanka"
+print("Name:", name, "Type:", type(name))
+
+# Integer
+age = 25
+print("Age:", age, "Type:", type(age))
+
+# Float
+height = 5.6
+print("Height:", height, "Type:", type(height))
+
+# Boolean
+is_student = True
+print("Is student:", is_student, "Type:", type(is_student))
+
+# List
+fruits = ["apple", "banana", "cherry"]
+print("Fruits:", fruits, "Type:", type(fruits))
+
+# Dictionary
+person = {"name": "Priyanka", "age": 25}
+print("Person:", person, "Type:", type(person))
