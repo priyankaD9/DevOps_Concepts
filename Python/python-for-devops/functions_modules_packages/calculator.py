f2f05702daf0459cf22a2dcc4099813721aa755e
Priyanka Dhage <dhagepriyanka938@gmail.com>
2026-04-10 06:11:36 +0000

    Added calculator functions

---
+++ Python/python-for-devops/functions_modules_packages/calculator.py
@@ -0,0 +1,18 @@
+
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    return a / b
+
+print("Addition:", add(3,6))
+print("Subtraction:", sub(8,4))
+print("Multiplication:", mul(2,4))
+print("Division:", div(12,6))
